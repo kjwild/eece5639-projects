2061c0ecb0941810e17da1452503214be44064f7
Kevin J Wu <wu.kevi@northeastern.edu>
2023-02-20 18:24:46 -0500

    Modify plotFilterResult to control which frames to display

--- project1/plotFilterResult.m
+++ project1/plotFilterResult.m
@@ -1,4 +1,4 @@
-function plotFilterResult(I,R,threshold,frame)
+function plotFilterResult(I,R,threshold,frame,showOutput)
 %%plotFilterResult Animate a series of images representing the filter result.
 %   plotFilterResult(I,R,THRESHOLD) takes the M-by-N-by-T array I of grayscale
 %   images -- where M and N are the image dimensions and T is the number of
@@ -10,15 +10,28 @@ function plotFilterResult(I,R,threshold,frame)
 %   The heatmap uses the jet RGB colormap.  Green indicates low to no response;
 %   red, high negative; and blue, high positive.
 %
-%   plotFilterResult(I,R,THRESHOLD,FRAME) stops the figure on FRAME.
+%   plotFilterResult(I,R,THRESHOLD,FRAME) returns only the data corresponding to
+%   the FRAME number of I only the 3rd dimension.  If the number of frames is
+%   greater than 3, then only one figure is output with DRAWNOW() called.
+%   Otherwise, only those frames are output in the corresponding number of
+%   figures.
+%
+%   plotFilterResult(...,SHOWOUTPUT) adjusts which results are shown in the
+%   output figure depending on what values are in SHOWOUTPUT.
+%       1:  original image
+%       2:  response heatmap
+%       3:  threshold response
 %
 %   Contact:        wu.kevi@northeastern.edu
-%   Last updated:   February 13, 2023
+%   Last updated:   February 20, 2023
 
 
 % Check input
-if (nargin < 4)
-    frame = 0;
+if (nargin < 4) || isempty(frame)
+    frame = 1:size(I,3);
+end
+if (nargin < 5)
+    showOutput = 1:3;
 end
 
 
@@ -42,38 +55,57 @@ Rnorm = round((R - newMinValue)*scaleFactor) + 1; % normalized values = indices
 
 % Create plot
 % - Figure
-figure;
+if (length(frame) > 3)
+    doAnimation = true;
+    fig = figure;
+else
+    doAnimation = false;
+    fig = cell(1,length(frame));
+    fig{1} = figure;
+end
 % - Time loop
-for i = 1:size(I,3)
+for i = frame
+    % -- Montage input buffer
+    montageInput = cell(1,3);
     % -- Image data
     imageData = I(:,:,i);
-    % -- Heatmap
-    responseColorIdx = reshape(Rnorm(:,:,i),1,[],1); % column index
-    responseColorVal = colorValue(responseColorIdx,:); % (M*N)-by-3 RGB values
-    heatmapMatrix = uint8(reshape(256*responseColorVal,m,n,3));
-    % -- Binary overlay
-    redValues = imageData;
-    greenValues = imageData;
-    blueValues = imageData;
-    switch length(threshold)
-        case 1
-            motionDetected = (abs(R(:,:,i)) > threshold);
-        case 2
-            motionDetected = (R(:,:,i) < threshold(1)) ...
-                | (R(:,:,i) > threshold(2));
+    if any(showOutput == 1)
+        montageInput{1} = imageData;
     end
-    redValues(motionDetected) = 0;
-    greenValues(motionDetected) = 0;
-    blueValues(motionDetected) = 255;
-    binaryOverlay = reshape([redValues greenValues blueValues],m,n,3);
+    % -- Heatmap
+    if any(showOutput == 2)
+        responseColorIdx = reshape(Rnorm(:,:,i),1,[],1); % column index
+        responseColorVal = colorValue(responseColorIdx,:); % (M*N)-by-3 RGB
+        heatmapMatrix = uint8(reshape(256*responseColorVal,m,n,3));
+        montageInput{2} = heatmapMatrix;
+    end
+    % -- Binary overlay
+    if any(showOutput == 3)
+        redValues = imageData;
+        greenValues = imageData;
+        blueValues = imageData;
+        switch length(threshold)
+            case 1
+                motionDetected = (abs(R(:,:,i)) > threshold);
+            case 2
+                motionDetected = (R(:,:,i) < threshold(1)) ...
+                    | (R(:,:,i) > threshold(2));
+        end
+        redValues(motionDetected) = 0;
+        greenValues(motionDetected) = 0;
+        blueValues(motionDetected) = 255;
+        binaryOverlay = reshape([redValues greenValues blueValues],m,n,3);
+        montageInput{3} = binaryOverlay;
+    end
     % -- Plot
-    montage({imageData heatmapMatrix binaryOverlay},'Size',[1 3]);
+    idx = ~cellfun(@isempty,montageInput);
+    montage(montageInput(idx),'Size',[1 length(showOutput)]);
     % -- Auxiliary
     title(['Frame ' num2str(i)]);
-    if any(i == frame)
-        keyboard();
-    else
+    if doAnimation
         drawnow();
+    elseif (i ~= frame(end))
+        fig{i == frame} = figure;
     end
 end
 
